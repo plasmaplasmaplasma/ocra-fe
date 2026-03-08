1f6e595f5a00746ce6be9bb9af19bd53f10e6695
plasmaplasma <l.paparo@studio.unibo.it>
2026-03-08 19:31:31 +0100

    init project

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["nuxt-auth-utils", "shadcn-nuxt"],
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
